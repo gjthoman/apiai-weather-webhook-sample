4538421e6f7c087ce04dbd929df6c251c5949b69
Greg Thoman <greg.thoman@daveramsey.com>
2017-03-23 13:13:51 -0500

    strip out yw

--- app.py
+++ app.py
@@ -60,27 +60,27 @@ def makeYqlQuery(req):
 
 
 def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
+    # query = data.get('query')
+    # if query is None:
+    #     return {}
 
-    result = query.get('results')
-    if result is None:
-        return {}
+    # result = query.get('results')
+    # if result is None:
+    #     return {}
 
-    channel = result.get('channel')
-    if channel is None:
-        return {}
+    # channel = result.get('channel')
+    # if channel is None:
+    #     return {}
 
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
+    # item = channel.get('item')
+    # location = channel.get('location')
+    # units = channel.get('units')
+    # if (location is None) or (item is None) or (units is None):
+    #     return {}
 
-    condition = item.get('condition')
-    if condition is None:
-        return {}
+    # condition = item.get('condition')
+    # if condition is None:
+    #     return {}
 
     # print(json.dumps(item, indent=4))
 
